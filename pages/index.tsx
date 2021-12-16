b54a4431985ca63e0c80289c838e432bc38f3ce7
toluboy <tolualonge123@gmail.com>
2021-12-16 17:17:25 +0100

    small market place section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography,Grid,Container } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import styles from '../styles/Home.module.css'
 
@@ -47,11 +47,45 @@ const Home: NextPage = () => {
         </div>
       </section>
 
+      <section className={styles.marketPlace }> 
+      <div className={styles.marketHeading} > 
+      <Button variant="outlined" className={styles.marketButton}>HOW THE MARKETPLACE WORKS</Button>
+      </div>
+      <p className={styles.content}>CDcare allows an instalmental payment plan for your preferred and in-demand gadgets and appliances. </p>
+  
+      <Grid container spacing={2}>
 
-<section className="marketPlace">
+    <Grid item xs={5}>
+    <Image
+                src="/cdmobile.png"
+                alt="Picture of cdcare logo"
+                width={388}
+                height={540}
+              />
+    </Grid>
 
-</section>
+    <Grid item xs={7}>
+  <div className={styles.space} >
+  <li className={styles.listHeading}> Spread your cost</li>
+    <p className={styles.listText}>We help you to spread the cost into equal weekly
+or monthly payments towards buying items over a given period of time.</p>
+  </div>
+  <div>
+  <li className={styles.listHeading}> Mid-Payment Delivery</li>
+    <p className={styles.listText}>Items are delivered before full payment in the middle of the installment period when
+     half of the instalment amount has been paid.</p>
+  </div>
+
+  <div>
+  <li className={styles.listHeading}> After-delivery Payment</li>
+    <p className={styles.listText}>The installment payments continue after delivery until full payments are made.</p>
+  </div>
+    </Grid>
+
+  </Grid>
+  
       
+  </section>
     </div>
   );
 };
